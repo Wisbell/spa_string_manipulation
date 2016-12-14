46677845c828a5144ad669a5e03272e5a71ea28f
Wisbell <westleyisbell@gmail.com>
2016-12-13 18:45:21 -0600

    Created my own function to check for all alpha characters in the input

--- strings.js
+++ strings.js
@@ -16,11 +16,27 @@ alphabits(testString);
 palindrome(testString);
 
 
+
+function checkInputAllAlpha () {
+
+    var regexExp = /[A-Za-z]/g
+
+    console.log("blank", document.querySelector('input[name="stringHere"]').value.replace(regexExp, ''))
+
+    if (document.querySelector('input[name="stringHere"]').value.replace(regexExp, '') === "") {
+        console.log("what")
+    }
+}
+
+
 function applyEventListeners() {
 
     document.querySelector('.what').addEventListener('click', function(event) {
         event.preventDefault()
         console.log("What button clicked")
+
+        // Check if input contains only letters
+        checkInputAllAlpha()
     })
 }
 
