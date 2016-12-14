56627515ccb7eb2294871f7412049ff27d85e05e
Wisbell <westleyisbell@gmail.com>
2016-12-13 19:03:10 -0600

    Added comments to main functions and added event listener on the input element for the enter key

--- strings.js
+++ strings.js
@@ -1,14 +1,29 @@
+/*
+Implement the logic in the reversal function that reverses the order of the
+characters in the string, and outputs the result in the DOM, below the text input.
+*/
 function reversal(string) {
 
 }
 
+
+/*
+Implement the logic in the alphabits function that return the characters in
+alphabetical order, and outputs the result in the DOM, below the text input.
+*/
 function alphabits(string) {
 
 }
 
+
+/*
+Implement the logic in the palindrome function that determine whether the
+string is a palindrome. If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
+*/
 function palindrome(string) {
 
 }
+
 
 var testString = "";
 reversal(testString);
@@ -43,13 +58,30 @@ function applyEventListeners() {
         console.log("button clicked")
 
         // Get input value
-        var inputString = document.querySelector('input[name="stringHere"]').value
+        testString = document.querySelector('input[name="stringHere"]').value
 
         // Check if input contains only letters and run all functions if true
-        if(checkInputAllAlpha(inputString)){
-            main(inputString)
+        if(checkInputAllAlpha(testString)){
+            main(testString)
         } else {
             alert("Input value contains non-alphanumeric characters.  The input only accepts alphabetical characters")
+        }
+    })
+
+
+    document.querySelector('input[name="stringHere"]').addEventListener('keypress', function(event){
+
+        if(event.keyCode === '13'){
+
+            // Get input value
+            testString = document.querySelector('input[name="stringHere"]').value
+
+            // Check if input contains only letters and run all functions if true
+            if(checkInputAllAlpha(testString)){
+                main(testString)
+            } else {
+                alert("Input value contains non-alphanumeric characters.  The input only accepts alphabetical characters")
+            }
         }
     })
 }
