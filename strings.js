5c4c585bee223bc99a7caa1079a536ec91e12b07
Wisbell <westleyisbell@gmail.com>
2016-12-13 18:05:19 -0600

    Added basic html elements and created event listener for button

---
+++ strings.js
@@ -0,0 +1,24 @@
+function reversal() {
+
+}
+
+function alphabits() {
+
+}
+
+function palindrome() {
+
+}
+
+var testString = "";
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
+
+
+function applyEventListeners() {
+
+    document.querySelector('.what').addEventListener('click', function() {
+        console.log("What button clicked")
+    })
+}
